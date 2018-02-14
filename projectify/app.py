11a371f0ad9786ba701f860b6fa1c933f83a9de7
Aaron Biliyok <abiliyok@gmail.com>
2018-02-14 21:14:45 +0100

    add config to the base application

--- projectify/app.py
+++ projectify/app.py
@@ -4,6 +4,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+
     @app.route('/')
     def index():
         return 'Welcome to projectify api'
