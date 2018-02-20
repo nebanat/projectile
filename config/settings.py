10eb3e3240e00ed80a1608d3e1812578c088b218
Aaron Biliyok <abiliyok@gmail.com>
2018-02-20 15:53:44 +0100

    chore(setup postgres service): setup postgres service in docker container - add postgres service to docker - initialize app with sqlalchemy as app orm [delivers 155329799]

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,8 @@
 DEBUG = True
 
 SERVER_NAME = 'localhost:8000'
+
+# SQLAlchemy.
+db_uri = 'postgresql://andeladeveloper:root234@postgres:5432/projectify'
+SQLALCHEMY_DATABASE_URI = db_uri
+SQLALCHEMY_TRACK_MODIFICATIONS = False
