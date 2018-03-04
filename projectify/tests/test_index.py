aedae116dd42927ddc28583d34faa824401c99be
Aaron Biliyok <abiliyok@gmail.com>
2018-03-04 14:57:05 +0100

    chore(config test database): configure a separate database for test - separate test db from development [delivers 155660176))]

--- projectify/tests/test_index.py
+++ projectify/tests/test_index.py
@@ -2,7 +2,7 @@ from flask import url_for
 
 
 class TestIndex(object):
-    def test_index_page(self,client):
+    def test_index_page(self, client):
         """first test for index page"""
         response = client.get(url_for('index'))
         assert response.status_code == 200
